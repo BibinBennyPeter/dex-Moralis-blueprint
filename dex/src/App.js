f776387f731c37be62eb096b92e817a4567da3b5
Bibin Benny Peter <bibinbennypeter@gmail.com>
2025-01-03 18:40:31 +0530

    create swap page

--- dex/src/App.js
+++ dex/src/App.js
@@ -1,7 +1,20 @@
+import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Swap from "./components/Swap";
+import Tokens from "./components/Tokens";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Dex</div>;
+  const [isConnected, setIsConnected] = React.useState(true);
+  return <div className="App">      
+  <Header connect={true} isConnected={isConnected} address={'0x8bvI9N4830jfSDN844J3dJ42e28'} />
+  <div className="mainWindow">
+    <Routes>
+      <Route path="/" element={<Swap isConnected={isConnected} address={'0x8bvI9N4830jfSDN844J3dJ42e28'} />} />
+      <Route path="/tokens" element={<Tokens />} />
+    </Routes>
+  </div></div>;
 }
 
 export default App;
